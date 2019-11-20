bcd7497ee360f309c8ddc4b5cced687e632200b5
Ramazan <rkhalbaginov@mail.ru>
2019-11-20 22:00:15 +0300

    йцукен

---
+++ src/components/Exchange.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="exchange">
+        <div class="error" v-if="errorMessage">
+            {{errorMessage}}
+        </div>
+        <div class="table__b" v-else>
+            <b-table striped hover :items="list"></b-table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapState, mapActions} from 'vuex';
+    export default {
+        name: "Exchange",
+        computed: {
+            ...mapState(['list', 'errorMessage'])
+        },
+        methods: {
+            ...mapActions(['init'])
+        },
+        created() {
+            this.init();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
+const http = axios.create({
+    baseURL: 'https://api.kraken.com/0/public/Ticker?pair=ETHUSD,ETHEUR,ETHCAD,ETHJPY,ETHGBP',
+    timeout: 1000,
+});
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        list:[],
+        errorMessage: null,
+    },
+    mutations: {
+        fillList(state, list) {
+            for(let item in list) {
+                state.list.push({
+                        Pair: item.replace('Z', '-').replace('X', ''),
+                        Bid: list[item].b[0],
+                        Ask: list[item].a[0]
+                    })
+            }
+        },
+        throwErrorMessage(state, message) {
+            state.errorMessage = message;
+        }
+    },
+    actions: {
+        async init(context) {
+            try {
+                let res = await http.get();
+                if( res.data.error.length ) {
+                    context.commit('throwErrorMessage', res.data.error[0]);
+                } else {
+                    context.commit('fillList', res.data.result);
+                }
+            } catch (e) {
+                context.commit('throwErrorMessage', e);
+            }
+
+        }
+    },
 })
